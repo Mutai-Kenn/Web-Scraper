58eb1109c418f3b6860e98ad4084c90a2aa86d17
Kenneth Mutai <mutaikenn@gmail.com>
2021-10-28 14:17:57 +0300

    created a function to scrap data from coinmarketcap

--- index.js
+++ index.js
@@ -4,31 +4,111 @@ const cheerio = require('cheerio');
 
 const PORT = 3000
 
-
 const app = express()
 
-const url = 'https://www.bbc.com/';
+// Getting data from the url site function
+async function getPriceFeed(){
+    try{
+        const url = 'https://coinmarketcap.com/';
 
-axios(url)
-    .then(response => {
-        const html= response.data
-        const $ = cheerio.load(html)
-        const articles = []
+        // Get data
+        const { data } = await axios({
+            method: "GET",
+            url: url,
+        })
+        
+        const $ = cheerio.load(data)
+        const elementSelector = '.h7vnx2-2 > tbody:nth-child(3) > tr'
 
+        const keys = [
+            'rank',
+            'name',
+            'price',
+            '24h',
+            '7d',
+            'marketCap',
+            'volume',
+            'circulatingSupply'
+        ]
 
-        $('.media__link', html).each(function(){
-           const title = $(this).text()
-           const link = $(this).attr('href')
+        const coinArr = []
 
-           articles.push({
-            title,
-            link
-          
-              
-           })
-            console.log(articles);
+        $(elementSelector).each((parentIdx, parentElem) => {
+            let keyIndex = 0
+            const coinObj = {}
+
+            if(parentIdx <= 9) {
+                console.log(parentIdx)
+                $(parentElem).children().each((childIdx, childElem) => {
+                    let tdValue = $(childElem).text()
+
+                    if(keyIndex === 1 || keyIndex === 6){
+                        tdValue = $('p:first-child', $(childElem).html()).text()
+                    }
+
+                    if(tdValue){
+                        coinObj[keys[keyIndex]] = tdValue
+
+                        keyIndex++
+                    }
+                    
+                })
+               coinArr.push(coinObj)
+            }
 
         })
-    })
+        return coinArr
+        
+    }catch(err){
+        console.error(err)
+    }
+
+}
+
+app.get('/', async (req,res) => {
+    try{
+        const priceFeed = await getPriceFeed()
+        return res.status(200).json({
+            result: priceFeed,
+        })
+
+    }catch(err){
+        return res.status(500).json({
+            err: err.toString(),
+        })
+        
+    }
+})
 
 app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`));
+
+
+
+
+
+//getPriceFeed()
+
+
+
+
+// axios(url)
+//     .then(response => {
+//         const html= response.data
+//         const $ = cheerio.load(html)
+//         const articles = []
+
+
+//         $('.media__title', html).each(function(){
+//            const title = $(this).text().trim()
+//            const link = $(this).find('a').attr('href')
+
+//            articles.push({
+//             title,
+//             link
+//            })
+//             console.log(articles);
+
+//         })
+//     })
+
+// app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`));
